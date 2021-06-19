e10a47db8cbfae893cd848656d348d4fcaaa8943
Danielle Martin <danielle.jaclyn.martin@gmail.com>
2021-06-19 13:04:00 -0700

    build table using data.js data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Build the table with data stored in data.js
+tableData.forEach(([datetime, city, state, country, shape, durationMinutes, comments]) => {
+	var row = tbody.append("tr")
+	row.append("td").text(datetime)
+	row.append("td").text(city)
+    row.append("td").text(state)
+    row.append("td").text(country)
+    row.append("td").text(shape)
+    row.append("td").text(durationMinutes)
+    row.append("td").text(comments)
+})
