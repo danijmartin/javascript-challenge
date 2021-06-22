4953328bb64b329cb395cbff13364b84fe82467c
Danielle Martin <danielle.jaclyn.martin@gmail.com>
2021-06-21 22:53:23 -0700

    created working filter function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -19,4 +19,38 @@ tableData.forEach(function(ufoSightings) {
 var button = d3.select("#filter-btn");
 
 // 2. Create Event Handler
-button.on("click", runEnter);
+button.on("click", filterDate);
+
+// 3. Create the function to run event
+function filterDate() {
+
+    // Grab input element
+    var inputElement = d3.select(".form-control");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    // console.log(inputValue) - Used to check code
+
+    // Filter data using inputValue if inputValue not empty
+    if (inputValue !== "") {
+        var filteredData = tableData.filter(input => input.datetime === inputValue);
+    }
+    else 
+        {var filteredData = tableData;
+    }
+    console.log(filteredData)
+
+    // Clear previous table
+    tbody.selectAll('tr').remove()
+
+    // Rebuild table with filtered Data
+    filteredData.forEach(function(ufoSightings) {
+        var row = tbody.append("tr");
+        Object.entries(ufoSightings).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
+
+}
